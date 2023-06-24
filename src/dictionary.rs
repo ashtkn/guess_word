4da638918103d65d0d1b43ad52396615a93f99f2
Asahi Takenouchi <asahi.taken@gmail.com>
2023-06-24 17:53:28 +0900

    refactor: separate dictionary module

---
+++ src/dictionary.rs
@@ -0,0 +1,21 @@
+const WORDS: &str = include_str!("words.txt");
+
+use rand::seq::SliceRandom;
+use std::collections::HashSet;
+
+pub struct Dictionary {
+    pub words: HashSet<&'static str>,
+}
+
+impl Dictionary {
+    pub fn new() -> Self {
+        let words: HashSet<&str> = WORDS.split("\n").collect();
+        Self { words }
+    }
+    pub fn get_random_words(&self) -> String {
+        Vec::from_iter(self.words.iter())
+            .choose(&mut rand::thread_rng())
+            .unwrap()
+            .to_string()
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,31 +1,63 @@
-const WORDS: &str = include_str!("words.txt");
+pub mod dictionary;
+use dictionary::Dictionary;
+use std::collections::HashMap;
 
-use std::collections::{HashMap, HashSet};
-
-use rand::seq::SliceRandom;
-
-pub struct Dictionary {
-    words: HashSet<&'static str>,
-}
-
-impl Dictionary {
-    pub fn new() -> Self {
-        let words: HashSet<&str> = WORDS.split("\n").collect();
-        Self { words }
-    }
-    pub fn get_random_words(&self) -> String {
-        Vec::from_iter(self.words.iter())
-            .choose(&mut rand::thread_rng())
-            .unwrap()
-            .to_string()
-    }
-}
+pub const GUESS_LENGTH: usize = 5; // 単語の文字列
+pub const GUESS_MAX: usize = 6; // 推理の試行最大数
 
 pub struct Game {
     guesses: Vec<WordGuess>,
     answer: String,
     game_status: GameStatus,
     dictonary: Dictionary,
+}
+
+#[derive(Copy, Clone, Debug, PartialEq, Eq, PartialOrd)]
+pub enum HitAccuracy {
+    InRightPlace, // 位置が正しい
+    InWord,       // 単語に含まれている
+    NotInWord,    // 単語に含まれていない
+}
+
+#[derive(Copy, Clone, Debug, PartialEq, Eq)]
+pub struct GuessLetter {
+    pub letter: char,
+    pub accuracy: HitAccuracy,
+}
+
+#[derive(Debug, PartialEq, Eq)]
+pub struct WordGuess {
+    pub letters: Vec<GuessLetter>,
+}
+
+#[derive(Copy, Clone, Debug, PartialEq, Eq)]
+pub enum GuessResult {
+    DuplicateGuess,  // 推理単語が重複している
+    IncorrectLength, // 文字数が不正
+    NotInDictionary, // 単語辞書にない
+    Valid,           // 有効
+    GameOver,        // すでにゲームが終了している
+}
+
+#[derive(Copy, Clone, Debug, PartialEq, Eq)]
+pub enum GameStatus {
+    Won,        // 勝利（推理が当たった）
+    InProgress, // 推理中
+    Lost,       // 敗北（推理が全て当たらなかった）
+}
+
+#[derive(Debug, Clone)]
+pub enum GameError {
+    GameNotLostError,
+}
+
+impl WordGuess {
+    pub fn word(&self) -> String {
+        self.letters.as_slice().iter().map(|gl| gl.letter).collect()
+    }
+    pub fn letters(&self) -> &[GuessLetter] {
+        self.letters.as_slice()
+    }
 }
 
 impl Default for Game {
@@ -54,35 +86,31 @@ impl Game {
     pub fn in_dictionary(&self, word: &str) -> bool {
         self.dictonary.words.get(word).is_some()
     }
-}
+    pub fn guess(&mut self, guess_input: &str) -> (GameStatus, GuessResult) {
+        if guess_input.len() != GUESS_LENGTH {
+            return (self.game_status, GuessResult::IncorrectLength);
+        }
+        if self.guess_already_exists(guess_input) {
+            return (self.game_status, GuessResult::DuplicateGuess);
+        }
+        if !self.in_dictionary(guess_input) {
+            return (self.game_status, GuessResult::NotInDictionary);
+        }
 
-pub const GUESS_LENGTH: usize = 5; // 単語の文字列
-pub const GUESS_MAX: usize = 6; // 推理の試行最大数
+        let guess = self.build_guess(guess_input);
+        self.guesses.push(guess);
 
-#[derive(Copy, Clone, Debug, PartialEq, Eq, PartialOrd)]
-pub enum HitAccuracy {
-    InRightPlace, // 位置が正しい
-    InWord,       // 単語に含まれている
-    NotInWord,    // 単語に含まれていない
-}
-
-#[derive(Copy, Clone, Debug, PartialEq, Eq)]
-pub struct GuessLetter {
-    pub letter: char,
-    pub accuracy: HitAccuracy,
-}
-
-#[derive(Debug, PartialEq, Eq)]
-pub struct WordGuess {
-    pub letters: Vec<GuessLetter>,
-}
-
-impl WordGuess {
-    pub fn word(&self) -> String {
-        self.letters.as_slice().iter().map(|gl| gl.letter).collect()
+        if guess_input == self.answer {
+            self.game_status = GameStatus::Won;
+            return (self.game_status, GuessResult::Valid);
+        }
+        if self.guesses.len() == GUESS_MAX {
+            self.game_status = GameStatus::Lost;
+        }
+        (self.game_status, GuessResult::Valid)
     }
-    pub fn letters(&self) -> &[GuessLetter] {
-        self.letters.as_slice()
+    pub fn game_status(&self) -> GameStatus {
+        self.game_status
     }
 }
 
@@ -99,18 +127,12 @@ impl Game {
         }
         counts
     }
-}
-
-impl Game {
     fn answer_char_at_index(&self, index: usize) -> char {
         self.answer.chars().nth(index).unwrap()
     }
     fn matches_answer_at_index(&self, index: usize, letter: char) -> bool {
         letter == self.answer_char_at_index(index)
     }
-}
-
-impl Game {
     fn build_guess_letter_with_accuracy(
         &mut self,
         letter_index: usize,
@@ -140,9 +162,6 @@ impl Game {
         };
         GuessLetter { letter, accuracy }
     }
-}
-
-impl Game {
     fn build_guess(&mut self, guess_input: &str) -> WordGuess {
         let mut available_letters = self.build_letter_counts(&self.answer);
         let mut guess_letters: Vec<Option<GuessLetter>> = vec![None; GUESS_LENGTH];
@@ -163,66 +182,10 @@ impl Game {
             letters: guess_letters.iter().map(|o| o.unwrap()).collect(),
         }
     }
-}
-
-impl Game {
     fn guess_already_exists(&self, guess_input: &str) -> bool {
         self.guesses
             .iter()
             .map(|g| g.word())
             .any(|x| x.eq(guess_input))
-    }
-}
-
-impl Game {
-    pub fn guess(&mut self, guess_input: &str) -> (GameStatus, GuessResult) {
-        if guess_input.len() != GUESS_LENGTH {
-            return (self.game_status, GuessResult::IncorrectLength);
-        }
-        if self.guess_already_exists(guess_input) {
-            return (self.game_status, GuessResult::DuplicateGuess);
-        }
-        if !self.in_dictionary(guess_input) {
-            return (self.game_status, GuessResult::NotInDictionary);
-        }
-
-        let guess = self.build_guess(guess_input);
-        self.guesses.push(guess);
-
-        if guess_input == self.answer {
-            self.game_status = GameStatus::Won;
-            return (self.game_status, GuessResult::Valid);
-        }
-        if self.guesses.len() == GUESS_MAX {
-            self.game_status = GameStatus::Lost;
-        }
-        (self.game_status, GuessResult::Valid)
-    }
-}
-
-#[derive(Copy, Clone, Debug, PartialEq, Eq)]
-pub enum GuessResult {
-    DuplicateGuess,  // 推理単語が重複している
-    IncorrectLength, // 文字数が不正
-    NotInDictionary, // 単語辞書にない
-    Valid,           // 有効
-    GameOver,        // すでにゲームが終了している
-}
-
-#[derive(Copy, Clone, Debug, PartialEq, Eq)]
-pub enum GameStatus {
-    Won,        // 勝利（推理が当たった）
-    InProgress, // 推理中
-    Lost,       // 敗北（推理が全て当たらなかった）
-}
-
-#[derive(Debug, Clone)]
-pub enum GameError {
-    GameNotLostError,
-}
-
-impl Game {
-    pub fn game_status(&self) -> GameStatus {
-        self.game_status
     }
 }
